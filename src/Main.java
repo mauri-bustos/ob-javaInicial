d86de1e2dbcc11fd8127aed32a55376ad25a02e5
Mauricio Bustos <mauricio.n.bustos@gmail.com>
2023-06-21 17:16:31 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		while(count < 10) {
+			
+			System.out.println(count + "numero");
+			count++;
+			
+		}
+		
+	}
+
+}
